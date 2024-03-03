760db475dfbd53f6f4905a79819d47f3736cc433
smagles <lesialist@icloud.com>
2024-03-03 12:31:01 +0100

    Rename service user

--- src/main/java/com/everybuy/ServiceUserDataApplication.java
+++ src/main/java/com/everybuy/ServiceUserDataApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceAuthorityApplication {
+public class ServiceUserDataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceAuthorityApplication.class, args);
+		SpringApplication.run(ServiceUserDataApplication.class, args);
 	}
 
 }
